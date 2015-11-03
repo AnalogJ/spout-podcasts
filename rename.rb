de75b4f5b74246ac65daa53ea808d84f8b80415a
Jason Kulatunga <jk17@ualberta.ca>
2015-11-02 18:16:18 -0800

    convert segments to array instead of a hash. Add uuid to all podcasts.

--- rename.rb
+++ rename.rb
@@ -1,19 +1,25 @@
 require 'fileutils'
 require 'pathname'
+require 'securerandom'
+require 'json'
 
 Dir.glob("**/*.json").each do |episode_path|
   if File.basename(episode_path) == 'metadata.json'
     next
   end
 
-  episode_filename = File.basename(episode_path,'.json')
-  episode_filename.strip!
-  episode_filename = episode_filename.gsub(/\s+/, '-')
+  episode_data = JSON.parse(File.read(episode_path))
 
-  pn = Pathname.new(episode_path)
+  episode_data['segments'] = episode_data['segments'].reduce([]) {|memo, (key, val)|
 
+    val['uuid'] = SecureRandom.uuid
 
+    memo.push(val)
+  }.sort! { |a,b| a['timestamp'] <=> b['timestamp'] }
+  p episode_data['segments']
+
+  File.write(episode_path, JSON.pretty_generate(episode_data))
   #p "#{episode_path} => #{pn.dirname}/#{episode_filename}.json"
-  File.rename(episode_path, "#{pn.dirname}/#{episode_filename}.json")
+  #File.rename(episode_path, "#{pn.dirname}/#{episode_filename}.json")
 
 end
